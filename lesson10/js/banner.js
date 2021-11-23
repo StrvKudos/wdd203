9f52614c3dc0bd0e7a15aead7427fa2d30c8cf35
StrvKudos <90884704+StrvKudos@users.noreply.github.com>
2021-11-24 07:01:39 +0800

    G

---
+++ lesson10/js/banner.js
@@ -0,0 +1,14 @@
+// This is the banner
+let banner = document.getElementById("banner");
+if (dow=="Friday" && 'https://jdbench.github.io/wdd230/lesson11/preston.html' == window.location.href){
+  banner.style.display = "flex";
+}else if (dow=="Saturday" && 'https://jdbench.github.io/wdd230/lesson11/sodasprings.html' == window.location.href){
+  banner.style.display = "flex";
+}else if (dow=="Thursday" && 'https://jdbench.github.io/wdd230/lesson11/fishhaven.html' == window.location.href){
+  banner.style.display = "flex";
+}
+function exitBanner(){
+if (banner.style.display === "flex") {
+  banner.style.display = "none";
+}
+}
