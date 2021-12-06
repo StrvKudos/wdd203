b53a75d5a75ad10ff1e0e1a4f7fbee46c1862b7d
StrvKudos <90884704+StrvKudos@users.noreply.github.com>
2021-12-06 12:39:12 +0800

    G

---
+++ lesson8/js/scripts.js
@@ -0,0 +1,12 @@
+ function toggleMenu(){
+        console.log(document.getElementById("sally").classList);
+        document.getElementById("sally").classList.toggle("hide");
+   
+ }
+//const d = new Date();
+//document.getElementById("demo").innerHTML = d;
+    
+//funtion chill() {
+    //let second = document.getElementById("");
+    //let 
+//}
---
+++ lesson9/script/homevents.js
@@ -0,0 +1,41 @@
+const infoURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+
+fetch(infoURL)
+  .then((response) => {
+    return response.json();
+  })
+  .then((jsonObject) => {
+    const towns = jsonObject["towns"];
+    towns.forEach((town) => {
+      if (
+        town.name == "Preston" ||
+        town.name == "Soda Springs" ||
+        town.name == "Fish Haven"
+      ) {
+      
+        let section = document.createElement("section");
+        let h3 = document.createElement("h3");
+        let divEvent = document.createElement("span");
+        let events = town.events;
+        let allEvents = document.createElement("span");
+
+
+        section.setAttribute("class", "town-event");
+        divEvent.setAttribute("class", "events");
+
+        h3.textContent = town.name;
+
+        events.forEach((event) => {
+          let eventText = document.createElement("span");
+          eventText.textContent = event;
+          allEvents.append(eventText);
+        });
+
+        allEvents.setAttribute("class", "events-name");
+        h3.setAttribute("class","h3-title");
+        divEvent.append(allEvents);
+        section.append(h3, divEvent);
+        document.querySelector("div.events-content").append(section);
+      }
+    });
+  });
